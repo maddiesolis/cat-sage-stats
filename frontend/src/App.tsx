a8bee6e351331e72490d624948127ba6fd8626b5
maddiesolis <madelinesdeo@gmail.com>
2024-02-11 11:57:16 -0800

    file name change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Sprite } from './SpriteAnimation';
+import { Sprite } from './Sprite';
 
 // TODO: make responsive
 const PageContainerDiv = styled.div`
