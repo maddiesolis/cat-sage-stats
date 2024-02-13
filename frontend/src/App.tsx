77f0667ee36ee84bdd24c50a3037e4c5ad53ad06
maddiesolis <madelinesdeo@gmail.com>
2024-02-13 00:03:05 -0800

    re-introduced websocket logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-// import { useState } from 'react';
-// import { SpriteAnimation } from './components/SpriteAnimation';
+import { useState, useEffect } from 'react';
 import { BackgroundAnimation } from './components/BackgroundAnimation';
 import background from './spritesheets/background.png'
-// import hands from './spritesheets/hands.png'
+import hands from './spritesheets/hands.png'
 import styled from 'styled-components';
 import { InfoPopover } from './components/InfoPopover';
+import { SpriteAnimation } from './components/SpriteAnimation';
 
 const CanvasContainer = styled.div`
   display: flex;
@@ -14,45 +14,45 @@ const CanvasContainer = styled.div`
 `
 
 function App() {
-  // const [playerState, setPlayerState] = useState('none');
+  const [playerState, setPlayerState] = useState('none');
 
-  // const handleServerRequest = ({ serverPlayerState }: { serverPlayerState: string}) => {
-  //   setPlayerState(serverPlayerState);
-  // }
+  const handleServerRequest = ({ serverPlayerState }: { serverPlayerState: string}) => {
+    setPlayerState(serverPlayerState);
+  }
 
-//   useEffect(() => {
-//     const ws = new WebSocket('ws://shark-app-epd3d.ondigitalocean.app');
+  useEffect(() => {
+    const ws = new WebSocket('ws://shark-app-epd3d.ondigitalocean.app');
 
-//     // Error handler
-//     ws.onerror = (error) => {
-//         console.error('WebSocket error:', error);
-//     };
+    // Error handler
+    ws.onerror = (error) => {
+        console.error('WebSocket error:', error);
+    };
 
-//     // Listen for player state updates from the server
-//     ws.onmessage = (event) => {
-//         const data = JSON.parse(event.data);
-//         handleServerRequest({ serverPlayerState: data.playerState });
-//     };
+    // Listen for player state updates from the server
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        handleServerRequest({ serverPlayerState: data.playerState });
+    };
 
-//     // Close the WebSocket connection when the component unmounts
-//     return () => {
-//       if (ws.readyState === 1) {
-//         ws.close();
-//       }
-//     };
-// }, []);
+    // Close the WebSocket connection when the component unmounts
+    return () => {
+      if (ws.readyState === 1) {
+        ws.close();
+      }
+    };
+}, []);
 
 
-  // const handleAnimationEnd = () => {
-  //   setPlayerState('none');
-  // };
+  const handleAnimationEnd = () => {
+    setPlayerState('none');
+  };
 
   return (
     <>
       <InfoPopover/>
       <CanvasContainer>
         <BackgroundAnimation spriteWidth={2000} spriteHeight={2000} staggerFrames={5} animationSheet={background} canvasWidth={2000} canvasHeight={2000} numFrames={13}/>
-        {/* <SpriteAnimation playerState={playerState} spriteWidth={700} spriteHeight={700} staggerFrames={5} spriteSheet={hands} onAnimationEnd={handleAnimationEnd}/> */}
+        <SpriteAnimation playerState={playerState} spriteWidth={700} spriteHeight={700} staggerFrames={5} spriteSheet={hands} onAnimationEnd={handleAnimationEnd}/>
       </CanvasContainer>
     </>
   );
