dcddc615151743fa53cbd68e85a259ea1600b01e
maddiesolis <madelinesdeo@gmail.com>
2024-03-25 23:49:33 -0700

    updated info popover

--- frontend/src/components/InfoPopover.tsx
+++ frontend/src/components/InfoPopover.tsx
@@ -9,6 +9,7 @@ import {
 } from '@chakra-ui/react';
 import styled from 'styled-components';
 import { useMediaQuery } from '@chakra-ui/react';
+import '@fontsource-variable/dm-sans';
 
 const PopoverDiv = styled.div`
     position: fixed;
@@ -31,12 +32,27 @@ export const InfoPopover: React.FC<InfoPopoverProps> = ({ text }) => {
         <PopoverDiv>
             <Popover placement={popoverPlacement}>
                 <PopoverTrigger>
-                <Button fontSize={20}>About</Button>
+                <Button
+                    size={reachedMinBreakPoint ? 'md' : 'lg'}
+                    bg='#eff0f1' 
+                    _hover={{ bg: '#e6e7e9' }}
+                    fontFamily="'DM Sans Variable', sans-serif"
+                >
+                    About
+                </Button>
                 </PopoverTrigger>
-                <PopoverContent style={{boxShadow:"none"}} w={popoverSize} padding='0.5rem'>
+                <PopoverContent
+                    style={{boxShadow:"none"}}
+                    w={popoverSize} 
+                    padding='0.5rem'
+                >
                     <PopoverArrow />
                     <PopoverCloseButton />
-                    <PopoverBody fontSize={18}>
+                    <PopoverBody
+                        fontSize={reachedMinBreakPoint ? 'md' : 'lg'}
+                        mt={2} 
+                        fontFamily="'DM Sans Variable', sans-serif"
+                    >
                         {text}
                     </PopoverBody>
                 </PopoverContent>
